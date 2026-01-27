d37ab129f7599c8a5e9bd7a8cbc3076bd0ae9d9b
Kushagra Kalbhawar <90435588+Kushagra1A@users.noreply.github.com>
2026-01-27 12:03:52 +0530

    [242] [Time Beats: 100.00%] [Memory Beats: 47.94%] - LeetPush

--- Valid-Anagram.cpp
+++ Valid-Anagram.cpp
@@ -5,21 +5,22 @@
 5            return false;
 6        }
 7
-8        int freq[256] = {0};
-9
-10        for(int i = 0; i < s.size(); i++){
-11            int val = s[i];
-12            freq[val]++;
-13        }
-14
-15        for(int i = 0; i < t.size(); i++){
-16            int val = t[i];
-17            freq[val]--;
-18            if(freq[val] < 0){
-19                return false;
-20            }
-21        }
-22
-23        return true;
-24    }
-25};
+8        //int freq[256] = {0};// do we really need space of 256 just for alphabets? can we try to reduce size?
+9        int freq[26] = {0};
+10
+11        for(int i = 0; i < s.size(); i++){
+12            int val = s[i] - 97; //because alphabets start from a = 97
+13            freq[val]++;
+14        }
+15
+16        for(int i = 0; i < t.size(); i++){
+17            int val = t[i] - 97;
+18            freq[val]--;
+19            if(freq[val] < 0){
+20                return false;
+21            }
+22        }
+23
+24        return true;
+25    }
+26};
