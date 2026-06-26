9126cb1849502490cb5a4f361ed35769baa53a3f
Kushagra Kalbhawar <90435588+Kushagra1A@users.noreply.github.com>
2026-06-26 11:13:24 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 48.26%] - LeetPush

--- Sort-Colors.cpp
+++ Sort-Colors.cpp
@@ -2,40 +2,24 @@
 2public:
 3    void sortColors(vector<int>& nums) {
 4        
-5        int cnt_zero = 0;
-6        int cnt_one = 0;
-7        int cnt_two = 0;
-8
-9        int n = nums.size();
-10
-11        for(int i = 0; i < n; i++){
-12            if(nums[i] == 0){
-13                cnt_zero++;
-14            }
-15            else if(nums[i] == 1){
-16                cnt_one++;
-17            }
-18            else{
-19                cnt_two++;
-20            }
-21        }
-22
-23        for(int i = 0; i < n; i++){
-24            if(cnt_zero != 0){
-25                nums[i] = 0;
-26                cnt_zero--;
-27                continue;
-28            }
-29            if(cnt_one != 0){
-30                nums[i] = 1;
-31                cnt_one--;
-32                continue;
-33            }
-34            if(cnt_two != 0){
-35                nums[i] = 2;
-36                cnt_two--;
-37                continue;
-38            }
-39        }
-40    }
-41};
+5        int n = nums.size();
+6        int low = 0; 
+7        int high = n - 1;
+8        int mid = 0;
+9
+10        while(mid <= high){
+11            if(nums[mid] == 0){
+12                swap(nums[low], nums[mid]);
+13                low++;
+14                mid++;
+15            }
+16            else if(nums[mid] == 1){
+17                mid++;
+18            }
+19            else{
+20                swap(nums[high], nums[mid]);
+21                high--;
+22            }
+23        }
+24    }
+25};
