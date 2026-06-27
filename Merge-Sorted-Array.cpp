6994c3948220ad7ef2788e45eca60c915ad31a76
Kushagra Kalbhawar <90435588+Kushagra1A@users.noreply.github.com>
2026-06-27 18:20:59 +0530

    [88] [Time Beats: 100.00%] [Memory Beats: 72.48%] - LeetPush

--- Merge-Sorted-Array.cpp
+++ Merge-Sorted-Array.cpp
@@ -2,12 +2,21 @@
 2public:
 3    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 4        
-5        int c = 0;
-6        for(int i = m; i < nums1.size(); i++){
-7            nums1[i] = nums2[c];
-8            c++;
-9        }
-10
-11        sort(nums1.begin(), nums1.end());
-12    }
-13};
+5        int p3 = m+n-1;
+6        int p1 = m-1;
+7        int p2 = n-1;
+8
+9        while(p2 >= 0){
+10            if(p1 >= 0 && nums1[p1] > nums2[p2]){
+11                nums1[p3] = nums1[p1];
+12                p3--;
+13                p1--;
+14            }
+15            else{//when nums1 > nums2
+16                nums1[p3] = nums2[p2];
+17                p3--;
+18                p2--;
+19            }
+20        }
+21    }
+22};
