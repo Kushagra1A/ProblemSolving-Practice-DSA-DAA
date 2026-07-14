4f21f828c08b93fe6eeb3882d41dbf7b43af190d
Kushagra Kalbhawar <90435588+Kushagra1A@users.noreply.github.com>
2026-07-14 12:54:49 +0530

    [1] [Time Beats: 42.56%] [Memory Beats: 12.22%] - LeetPush

--- 1-Two-Sum.cpp
+++ 1-Two-Sum.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mpp;
         int n = nums.size();
         vector<int>ans;
 
         for(int i = 0; i < n; i++){
-            int need = target - nums[i];
-            for(int j = i+1; j < n; j++){
-                if(nums[j] == need){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                } 
+            int curr = nums[i];
+            int rem = target - curr;
+
+            if(mpp.find(rem) != mpp.end()){
+                ans.push_back(i);
+                ans.push_back(mpp[rem]);
+                break;
+            }
+            else{
+                mpp[nums[i]] = i;
             }
         }
 
